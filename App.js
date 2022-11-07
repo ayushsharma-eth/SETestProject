df7c3d34e0d50ebba42ab56e1f3ff3dac0398490
Ayush Sharma <ayushsharma@Ayushs-MacBook-Pro.local>
2022-11-06 19:33:34 -0600

    init

---
+++ App.js
@@ -0,0 +1,102 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import Login from './screens/Login'
+
+export default function App() {
+  return (
+    <ScrollView>
+      <Text className="text-red">Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <View className="flex-1 items-center justify-center bg-white">
+        <Text>Epic App Building Time!</Text>
+      </View>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <Text>Epic App Building Time!</Text>
+      <StatusBar style="auto" />
+      <Login style={styles.login}/>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  login: {
+    backgroundColor: '#000'
+  }
+});
---
+++ screens/Login.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+function Login() {
+  return (
+    <View>
+      <Text>Epic App Building Time!</Text>
+    </View>
+  )
+}
+
+export default Login
