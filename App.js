30e93c21bfcf244c0e7cfd3014ba15bd7ef14692
Ayush Sharma <ayushsharma@Ayushs-MacBook-Pro.local>
2022-11-06 23:47:43 -0600

    comment on generalization

--- App.js
+++ App.js
@@ -5,12 +5,15 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen'
 import HomeScreen from './screens/HomeScreen';
 import RestaurantScreen from './screens/RestaurantScreen';
+import OrderScreen from './screens/OrderScreen';
 
 // App navigation use stack system, pages are push on top of another and can be popped off with back button
 const Stack = createNativeStackNavigator();
 
 // NOTE: Never push Maps API key onto GitHub
 
+
+// The Restaurant and Order Screens will be generalized with props
 export default function App() {
   return (
     <NavigationContainer>
@@ -18,6 +21,7 @@ export default function App() {
         <Stack.Screen name='Login' component={LoginScreen} options={{headerShown: false}}/>
         <Stack.Screen name='Home' component={HomeScreen} options={{headerShown: false}}/>
         <Stack.Screen name='Restaurant' component={RestaurantScreen} options={{headerShown: false}}/>
+        <Stack.Screen name='Order' component={OrderScreen} options={{headerShown: false}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ screens/OrderScreen.js
@@ -0,0 +1,12 @@
+import { ScrollView, Text, View, Pressable } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import MapView from 'react-native-maps';
+import { Marker } from 'react-native-maps';
+
+export default function OrderScreen() {
+  return (
+    <View>OrderScreen</View>
+  )
+}
