676befffa40e69e0bedd3c319f05cb6bb64fc813
Ayush Sharma <ayushsharma@Ayushs-MacBook-Pro.local>
2022-11-08 10:24:18 -0600

    testing branch

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -14,12 +14,15 @@ function HomeScreen({navigation}) {
         <Pressable onPress={() => navigation.navigate('Restaurant')} className="bg-white w-full items-center py-5 rounded-xl">
           <Text className="font-medium text-xl">Taco Bell</Text>
         </Pressable>
+        <Pressable onPress={() => navigation.navigate('Order')} className="bg-white w-full items-center py-5 rounded-xl">
+          <Text className="font-medium text-xl">View Active Order</Text>
+        </Pressable>
         <Pressable onPress={() => navigation.navigate('Login')} className="bg-white w-full items-center py-5 rounded-xl">
           <Text className="font-medium text-xl">Sign Out</Text>
         </Pressable>
 
         {/* Testing Map View for use in Order Screen and thumbnail view on Home Screen. These coords map to a Taco Bell. */}
-        <MapView
+        {/* <MapView
             initialRegion={{
             latitude: 33.12783688178995,
             longitude: -96.72796024170364,
@@ -31,7 +34,7 @@ function HomeScreen({navigation}) {
             <Marker
                 coordinate={{ latitude : 33.12783688178995 , longitude : -96.72796024170364 }}
             />
-        </MapView>
+        </MapView> */}
             
 
       </View>
--- screens/OrderScreen.js
+++ screens/OrderScreen.js
@@ -5,8 +5,30 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MapView from 'react-native-maps';
 import { Marker } from 'react-native-maps';
 
-export default function OrderScreen() {
+export default function OrderScreen({navigation}) {
   return (
-    <View>OrderScreen</View>
+    <View>
+        {/* Full Screen Map, centered on restaurant initially */}
+        <MapView
+            initialRegion={{
+            latitude: 33.12783688178995,
+            longitude: -96.72796024170364,
+            latitudeDelta: 0.000922,
+            longitudeDelta: 0.000421,
+            }}
+            className='w-full h-[90vh]'
+        >
+            <Marker
+                coordinate={{ latitude : 33.12783688178995 , longitude : -96.72796024170364 }}
+            />
+        </MapView>
+        <Pressable onPress={() => navigation.navigate('Home')} className="bg-white w-full items-center py-5 rounded-xl">
+          <Text className="font-medium text-xl">Home</Text>
+        </Pressable>
+
+        <View style={{position: 'absolute', left: 10, bottom: 10, justifyContent: 'center', alignItems: 'center'}}>
+            <Text className='text-5xl text-white font-bold'>Taco Bell</Text>
+        </View>
+    </View>
   )
 }
